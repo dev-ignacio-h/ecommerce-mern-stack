47b51f997cdd7ded3dba43b247df2c1be6e35aea
Ignacio <dev.ignacio.h@gmail.com>
2021-07-16 14:55:45 -0400

    initial commit
    initial setup
    frontend project with react
    create product component and rating component

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import data from './data';
+import Product from './components/Product'
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">
+            amazona
+          </a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div className="row center">
+          {data.products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </div>
+      </main>
+      <footer className="row center">All right reserverd</footer>
+    </div>
+  );
+}
+
+export default App;
